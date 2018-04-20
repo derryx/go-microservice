8d272644bec389665f8392e85b2a6b97a2318358
Thomas Peuss <thomas@peuss.de>
2018-04-20 15:18:20 +0200

    So der Test ist jetzt besser. Fixes #1

--- internal/pkg/webserver/webserver_test.go
+++ internal/pkg/webserver/webserver_test.go
@@ -9,13 +9,13 @@ import (
 func TestSpec(t *testing.T) {
 	// Only pass t into top-level Convey calls
 	Convey("Given some integer with a starting value", t, func() {
-		x := 2
+		x := 3
 
 		Convey("When the integer is incremented", func() {
 			x++
 
 			Convey("The value should be greater by one", func() {
-				So(x, ShouldEqual, 3)
+				So(x, ShouldEqual, 4)
 			})
 		})
 	})
